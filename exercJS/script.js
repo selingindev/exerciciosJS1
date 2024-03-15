82e5a92020ea1254efb35bce7027bbec2debfa5a
Arthur Selingin <88356338+selingindev@users.noreply.github.com>
2024-03-15 09:25:34 -0300

    subindo novamente

---
+++ exercJS/script.js
@@ -0,0 +1,20 @@
+/*var nome_do_carro = "Fusca"
+
+alert('Olá usuario digite seu nome ') 
+var nome = prompt();
+
+alert('Agora digite sua idade')
+var idade = prompt();
+
+alert('Ola usuario seu nome é: ' + nome + ' e sua idade é: ' + idade)
+*/
+
+valor1 = prompt()
+valor2 = prompt()
+
+var baseRetangulo = valor1
+var alturaRetangulo = valor2
+
+var retangulo = baseRetangulo * alturaRetangulo
+
+alert(retangulo)
